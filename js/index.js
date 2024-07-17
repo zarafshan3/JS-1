e205a25ca420a986b0f81c1abd4d23eddaa9a12e
unknown <ejazzarafshan@gmail.com>
2024-07-18 02:08:46 +0400

    javaisfun

---
+++ js/index.js
@@ -0,0 +1,37 @@
+function myFunction() {
+    // let a = confirm("Hi, this is Js Page :)")
+
+    // if (a){
+    //     document.body.style.background = "yellow"
+    //     document.body.style.color = "pink"
+    // } else {
+    //     alert("hehe, you've been pranked")
+    // }
+
+    let a = prompt("Please write 'Zara' :)")
+
+    if (a == "Zara") {
+        document.body.style.background = "Pink"
+    } else {
+        alert("hehe, you've been pranked")
+    }
+}
+
+let FirstName = "Zarafshan"
+let Surname = "Ejaz"
+let bDay = 2005
+let Current_year = 2024
+let Company = "Texnolab"
+let programming_language = "JavaScript"
+let Car = "Porsche"
+let School = "Education Trust Nasra School"
+let Country = "Azerbaijan"
+let Hobbies = "singing, painting, playing with my cats"
+let Pet = "Luna and Lily"
+let gender = "Female"
+let Movie = "Interstellar and Notebook"
+let From = "Pakistan"
+let game = "Pubg, Minecraft & Puzzle"
+
+console.log(`Hello my name is ${FirstName} ${Surname}. I am ${Current_year - bDay} years old ${gender} I am currently studying ${programming_language} at ${Company}. My favorite car is ${Car}. i live in ${Country} I am originally from ${From} I studied at ${School} in ${From}.
+     My hobbies are ${Hobbies}. I have two cats named ${Pet} My all time favorite movies to watch are ${Movie}. My favorite games to play are ${game} `)
